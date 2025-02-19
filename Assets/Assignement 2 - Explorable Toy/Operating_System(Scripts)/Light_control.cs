b56622dcfe24534917f89bc724c324d607220fc0
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-02-19 16:23:45 -0500

    -Added light switch connected to UI element -Also added UI foundations

---
+++ Assets/Assignement 2 - Explorable Toy/Operating_System(Scripts)/Light_control.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Light_control : MonoBehaviour
+{
+    bool lights = false;
+    SpriteRenderer darkness;
+    public AnimationCurve flicker;
+    [Range(0, 1)]
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        darkness = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color alpha = darkness.color;
+        alpha.a = Mathf.Lerp(0, 0.9f, flicker.Evaluate(t));
+        darkness.color = alpha;
+        
+        if (lights == true)
+        {
+           t -= 2 * Time.deltaTime;
+        }
+        
+        if(lights == false)
+        {
+            t += 2 * Time.deltaTime;
+        }
+    }
+
+    public void SwitchPressed()
+    {
+        lights = !lights;
+    }
+}
